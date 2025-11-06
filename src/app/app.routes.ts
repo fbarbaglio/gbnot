abf5265a451f250182461417387201d9599f799c
Federico Barbaglio <federico.barbaglio@dedagroup.it>
2025-11-06 09:52:15 +0100

    Add Servizi section with list of notary services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { NotaioComponent } from './notaio/notaio.component';
 import { DoveTrovarmiComponent } from './dove-trovarmi/dove-trovarmi.component';
 import { ContattiComponent } from './contatti/contatti.component';
+import { ServiziComponent } from './servizi/servizi.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'notaio', component: NotaioComponent },
   { path: 'dove-trovarmi', component: DoveTrovarmiComponent },
+  { path: 'servizi', component: ServiziComponent },
   { path: 'contatti', component: ContattiComponent }
 ];
